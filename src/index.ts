ea7bda58d25e8a54546b8075b284fc1e9361332f
andrejgusaninov <guschaninov@yandex.ru>
2022-05-11 17:00:16 +0300

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { AxiosRequestConfig } from "axios";
+import { AxiosRequestConfig, Method } from "axios";
 import { parse } from "qs";
 
 interface LimiterConfig {
@@ -9,7 +9,7 @@ async function getProxy(request: AxiosRequestConfig, config: LimiterConfig) {
   /*
       Необходимо иметь в request: proxyUrl
      */
-  let { baseURL = "", url = "", method = "get", params = "" } = request;
+  let { baseURL = "", url = "", params = "" } = request;
   const { proxyUrl = "" } = config;
 
   request.params = parse(params);
@@ -17,7 +17,7 @@ async function getProxy(request: AxiosRequestConfig, config: LimiterConfig) {
   request.headers = {
     ...request.headers,
     "amo-url": baseURL + url,
-    "amo-method": method,
+    "amo-method": request.method as Method,
   };
   request.method = "post";
 

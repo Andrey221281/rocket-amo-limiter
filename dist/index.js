b634bd52fde1f9d07d998c693695750778a23ddb
andrejgusaninov <guschaninov@yandex.ru>
2022-05-11 15:18:06 +0300

    fix method

--- dist/index.js
+++ dist/index.js
@@ -15,11 +15,12 @@ function getProxy(request, config) {
         /*
             Необходимо иметь в request: proxyUrl
            */
-        let { baseURL = "", url = "" } = request;
+        let { baseURL = "", url = "", method = "get", params = "" } = request;
         const { proxyUrl = "" } = config;
-        request.params = (0, qs_1.parse)(request.params);
+        request.params = (0, qs_1.parse)(params);
         request.baseURL = proxyUrl;
-        request.headers = Object.assign(Object.assign({}, request.headers), { amoUrl: baseURL + url });
+        request.headers = Object.assign(Object.assign({}, request.headers), { "amo-url": baseURL + url, "amo-method": method });
+        request.method = "post";
         return request;
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,17 @@ async function getProxy(request: AxiosRequestConfig, config: LimiterConfig) {
   /*
       Необходимо иметь в request: proxyUrl
      */
-  let { baseURL = "", url = "" } = request;
+  let { baseURL = "", url = "", method = "get", params = "" } = request;
   const { proxyUrl = "" } = config;
 
-  request.params = parse(request.params);
+  request.params = parse(params);
   request.baseURL = proxyUrl;
-  request.headers = { ...request.headers, amoUrl: baseURL + url };
+  request.headers = {
+    ...request.headers,
+    "amo-url": baseURL + url,
+    "amo-method": method,
+  };
+  request.method = "post";
 
   return request;
 }
